6cc23825feb355911422d56b158c06a9bf1abf04
YongwoonJang <royalfamily89@gmail.com>
2017-02-19 22:51:55 +0900

    Add files via upload

--- tetris.c
+++ tetris.c
@@ -13,8 +13,8 @@
 #include <GL/glu.h>
 #include <GL/glut.h>
 
-#define ROW 14
-#define COL 14
+#define ROW 24
+#define COL 24
 #define SPEED 300
 
 // Score
@@ -40,11 +40,11 @@ bool element[COL][ROW];
 // STOP : If block faces a bottom.
 // PROCEED : If block doesn't face a wall.
 enum CHECK{INIT, INIT_END,END,PROCEED,STOP};
-enum SHAPE{TYPE_A,TYPE_B,TYPE_C};
+enum SHAPE{TYPE_A,TYPE_B,TYPE_C,TYPE_D,TYPE_E,TYPE_F,TYPE_G};
 
 // Utility(Logic)
 // Check to proceed
-int checkProceed(int speed, int direction){
+int checkToProceed(int speed, int direction){
 	int result = INIT;
 	int diffX;
 	int diffY;
@@ -67,15 +67,18 @@ int checkProceed(int speed, int direction){
 	}
 
 	//Evaluating current algorithm.
-	if(type == TYPE_A){ // "--"
-		if(y <= - windowHalfHeight){
+	if(type == TYPE_A){
+		// "けけけけ"
+		if(y+diffY < - windowHalfHeight){
 			result = INIT_END;
 
-		}else if(((x+resize)+diffX) >= windowHalfWidth || (x+diffX) <= -(windowHalfWidth+1)){
+		}else if(((x+resize*3)+diffX) >= windowHalfWidth || (x+diffX) <= -(windowHalfWidth+1)){
 			result = STOP;
 
 		}else if(!element[(int)((y+diffY)+windowHalfHeight)][(int)((x+diffX)+windowHalfWidth)]
-															 &&!element[(int)((y+diffY)+windowHalfHeight)][(int)(((x+resize)+diffX)+windowHalfWidth)]){
+															 &&!element[(int)((y+diffY)+windowHalfHeight)][(int)(((x+resize)+diffX)+windowHalfWidth)]
+															 &&!element[(int)((y+diffY)+windowHalfHeight)][(int)(((x+resize*2)+diffX)+windowHalfWidth)]
+															 &&!element[(int)((y+diffY)+windowHalfHeight)][(int)(((x+resize*3)+diffX)+windowHalfWidth)]){
 			result = PROCEED;
 
 		}else{
@@ -84,14 +87,124 @@ int checkProceed(int speed, int direction){
 		}//end of if
 
 	}else if(type == TYPE_B){
-		if(y <= - windowHalfHeight){
+		//"け" shape
+		//"け" shape
+		//"け" shape
+		//"け" shape
+		if(y+diffY < - windowHalfHeight){
 					result = INIT_END;
 
 		}else if((x+diffX) >= windowHalfWidth || (x+diffX) <= -(windowHalfWidth+1)){
 			result = STOP;
 
 		}else if(!element[(int)((y+diffY)+windowHalfHeight)][(int)((x+diffX)+windowHalfWidth)]
+															 &&!element[(int)(((y+resize)+diffY)+windowHalfHeight)][(int)((x+diffX)+windowHalfWidth)]
+															 &&!element[(int)(((y+resize*2)+diffY)+windowHalfHeight)][(int)((x+diffX)+windowHalfWidth)]
+															 &&!element[(int)(((y+resize*3)+diffY)+windowHalfHeight)][(int)((x+diffX)+windowHalfWidth)]){
+			result = PROCEED;
+
+		}else{
+			result = STOP;
+
+		}//end of if
+
+	}else if(type == TYPE_C){
+
+		//"け" shape
+		//"け  け" shape
+		//"  け" shape
+		if((y-resize)+diffY < - windowHalfHeight){
+					result = INIT_END;
+
+		}else if(((x+resize)+diffX) >= windowHalfWidth || (x+diffX) <= -(windowHalfWidth+1)){
+			result = STOP;
+
+		}else if(!element[(int)((y+diffY)+windowHalfHeight)][(int)((x+diffX)+windowHalfWidth)]
+															 &&!element[(int)((y+diffY)+windowHalfHeight)][(int)(((x+resize)+diffX)+windowHalfWidth)]
+															 &&!element[(int)(((y+resize)+diffY)+windowHalfHeight)][(int)((x+diffX)+windowHalfWidth)]
+															 &&!element[(int)(((y-resize)+diffY)+windowHalfHeight)][(int)(((x+resize)+diffX)+windowHalfWidth)]){
+			result = PROCEED;
+
+		}else{
+			result = STOP;
+
+		}//end of if
+
+	}else if(type == TYPE_D){
+
+		//" けけ" shape
+		//"けけ " shape
+		if((y-resize)+diffY < - windowHalfHeight){
+					result = INIT_END;
+
+		}else if(((x+resize)+diffX) >= windowHalfWidth || ((x-resize)+diffX) <= -(windowHalfWidth+1)){
+			result = STOP;
+
+		}else if(!element[(int)((y+diffY)+windowHalfHeight)][(int)((x+diffX)+windowHalfWidth)]
+															 &&!element[(int)((y+diffY)+windowHalfHeight)][(int)(((x+resize)+diffX)+windowHalfWidth)]
+															 &&!element[(int)(((y-resize)+diffY)+windowHalfHeight)][(int)((x+diffX)+windowHalfWidth)]
+															 &&!element[(int)(((y-resize)+diffY)+windowHalfHeight)][(int)(((x-resize)+diffX)+windowHalfWidth)]){
+			result = PROCEED;
+
+		}else{
+			result = STOP;
+
+		}//end of if
+
+	}else if(type == TYPE_E){
+		//"  け" shape
+		//"け  け" shape
+		//"け     " shape
+		if((y-resize)+diffY < - windowHalfHeight){
+					result = INIT_END;
+
+		}else if((x+diffX) >= windowHalfWidth || ((x-resize)+diffX) <= -(windowHalfWidth+1)){
+			result = STOP;
+
+		}else if(!element[(int)((y+diffY)+windowHalfHeight)][(int)((x+diffX)+windowHalfWidth)]
+															 &&!element[(int)(((y-resize)+diffY)+windowHalfHeight)][(int)(((x-resize)+diffX)+windowHalfWidth)]
+															 &&!element[(int)((y+diffY)+windowHalfHeight)][(int)(((x-resize)+diffX)+windowHalfWidth)]
 															 &&!element[(int)(((y+resize)+diffY)+windowHalfHeight)][(int)((x+diffX)+windowHalfWidth)]){
+			result = PROCEED;
+
+		}else{
+			result = STOP;
+
+		}//end of if
+
+	}else if(type == TYPE_F){
+		//"け  け" shape
+		//"  け け   " shape
+		if((y-resize)+diffY < - windowHalfHeight){
+					result = INIT_END;
+
+		}else if(((x+resize)+diffX) >= windowHalfWidth || ((x-resize)+diffX) <= -(windowHalfWidth+1)){
+			result = STOP;
+
+		}else if(!element[(int)((y+diffY)+windowHalfHeight)][(int)((x+diffX)+windowHalfWidth)]
+															 &&!element[(int)((y+diffY)+windowHalfHeight)][(int)(((x-resize)+diffX)+windowHalfWidth)]
+															 &&!element[(int)(((y-resize)+diffY)+windowHalfHeight)][(int)((x+diffX)+windowHalfWidth)]
+															 &&!element[(int)(((y-resize)+diffY)+windowHalfHeight)][(int)(((x+resize)+diffX)+windowHalfWidth)]){
+			result = PROCEED;
+
+		}else{
+			result = STOP;
+
+		}//end of if
+
+	}else if(type == TYPE_G){
+		//"けけ"
+		//"けけ"
+		if(y+diffY < - windowHalfHeight){
+					result = INIT_END;
+
+		}else if(((x+resize)+diffX) >= windowHalfWidth || (x+diffX) <= -(windowHalfWidth+1)){
+			result = STOP;
+
+		}else if(!element[(int)((y+diffY)+windowHalfHeight)][(int)((x+diffX)+windowHalfWidth)]
+															 &&!element[(int)((y+diffY)+windowHalfHeight)][(int)(((x+resize)+diffX)+windowHalfWidth)]
+															 &&!element[(int)(((y+resize)+diffY)+windowHalfHeight)][(int)(x+windowHalfWidth)]
+															 &&!element[(int)(((y+resize)+diffY)+windowHalfHeight)][(int)(((x+resize)+diffX)+windowHalfWidth)]){
 			result = PROCEED;
 
 		}else{
@@ -118,6 +231,7 @@ void checkRow(){
 
 		//delete 1 line. // from I row.
 		if(k == COL){
+
 			for(int l = i; l < ROW; l++){
 				if(l == i){
 					for(int m = 0; m < COL ; m++){
@@ -174,9 +288,110 @@ void setupRC(void){
 
 //controller
 //key command receiver
+void normalKey(unsigned char key, int xi, int yi)
+{
+	int i = 0;
+	int result = 0;
+
+	switch(key){
+		case 13 ://"ISO Enter key code"
+			//Check how many to confront wall...
+			do{
+				i++;
+				result = checkToProceed(i, GLUT_KEY_DOWN);
+
+			}while(!(result==STOP)&&!(result==INIT_END));
+
+			y = y - (float)i +1;
+
+			break;
+
+		case 32 ://"ISO Space bar code"
+			if(type == TYPE_A){//"ぱ" to "び"
+				type = TYPE_B;
+
+			}else if(type == TYPE_B){//"l" to "ぱ"
+				//if fronting wall
+				if(x == (windowHalfWidth-resize)){
+					x = x-resize*3;
+					type = TYPE_A;
+
+				}else if(element[(int)(y+windowHalfHeight)][(int)(x+resize*3+windowHalfWidth)]==true
+						|| element[(int)(y+windowHalfHeight)][(int)(x+resize*2+windowHalfWidth)]==true
+						|| element[(int)(y+windowHalfHeight)][(int)(x+resize+windowHalfWidth)]==true){
+					type = TYPE_B;
+
+				}else{
+					type = TYPE_A;
+
+				}
+
+			}else if(type == TYPE_C){
+				//if fronting wall
+				if(x == (-windowHalfWidth)){
+					x = x+resize;
+					type = TYPE_D;
+
+				}else if(element[(int)((y-resize)+windowHalfHeight)][(int)(x+windowHalfWidth)]==true
+						|| element[(int)((y-resize)+windowHalfHeight)][(int)((x-resize)+windowHalfWidth)]==true){
+					type = TYPE_C;
+
+				}else{
+					type = TYPE_D;
+
+				}
+
+			}else if(type == TYPE_D){
+				//if fronting wall
+				if(element[(int)((y+resize)+windowHalfHeight)][(int)(x+windowHalfWidth)]==true
+						|| element[(int)((y-resize)+windowHalfHeight)][(int)((x+resize)+windowHalfWidth)]==true){
+					type = TYPE_D;
+
+				}else{
+					type = TYPE_C;
+
+				}
+
+			}else if(type == TYPE_E){
+				//if fronting wall
+				if(x == (windowHalfWidth-resize)){
+					x = x-resize;
+					type = TYPE_F;
+
+				}else if(element[(int)((y-resize)+windowHalfHeight)][(int)(x+windowHalfWidth)]==true
+						|| element[(int)((y-resize)+windowHalfHeight)][(int)((x+resize)+windowHalfWidth)]==true){
+					type = TYPE_E;
+
+				}else{
+					type = TYPE_F;
+
+				}
+			}else if(type == TYPE_F){
+				//if fronting wall
+				if(x == (windowHalfWidth)){
+					x = x-resize;
+					type = TYPE_E;
+
+				}else if(element[(int)((y+resize)+windowHalfHeight)][(int)(x+windowHalfWidth)]==true
+						|| element[(int)((y-resize)+windowHalfHeight)][(int)((x-resize)+windowHalfWidth)]==true){
+					type = TYPE_F;
+
+				}else{
+					type = TYPE_E;
+
+				}
+			}else if(type == TYPE_G){
+				//No transformation
+				type = TYPE_G;
+
+			}
+		break;
+	}
+}
+
 void specialKey(int key, int xi, int yi)
 {
-	switch(checkProceed(resize, key)){
+	switch(checkToProceed(resize, key)){
 		case PROCEED:
 			switch(key)
 				    {
@@ -194,6 +409,7 @@ void specialKey(int key, int xi, int yi)
 
 				        case GLUT_KEY_UP :
 				            break;
+
 				    }
 			break;
 
@@ -208,12 +424,23 @@ void specialKey(int key, int xi, int yi)
 
 //Randomly Find Block.
 void selectType(){
-	int sel = rand()%2;
+	int sel = rand()%5;
 
 	if(sel == 0){
 		type = TYPE_A;
+
 	}else if(sel == 1){
 		type = TYPE_B;
+
+	}else if(sel == 2){
+		type = TYPE_C;
+
+	}else if(sel == 3){
+		type = TYPE_E;
+
+	}else if(sel == 4){
+		type = TYPE_G;
+
 	}
 
 }
@@ -223,7 +450,7 @@ void animationTimer(int speed){
 
 
 
-	switch(checkProceed(speed, GLUT_KEY_DOWN)){
+	switch(checkToProceed(speed, GLUT_KEY_DOWN)){
 		case PROCEED:
 			//Process to downward
 			y = y - (GLfloat)speed;
@@ -234,12 +461,49 @@ void animationTimer(int speed){
 			if(type==TYPE_A){
 				element[(int)(y+windowHalfHeight)][(int)(x+windowHalfWidth)] = true;
 				element[(int)(y+windowHalfHeight)][(int)(x+resize+windowHalfWidth)] = true;
+				element[(int)(y+windowHalfHeight)][(int)(x+resize*2+windowHalfWidth)] = true;
+				element[(int)(y+windowHalfHeight)][(int)(x+resize*3+windowHalfWidth)] = true;
+
 			}else if(type==TYPE_B){
 				element[(int)(y+windowHalfHeight)][(int)(x+windowHalfWidth)] = true;
 				element[(int)(y+resize+windowHalfHeight)][(int)(x+windowHalfWidth)] = true;
+				element[(int)(y+resize*2+windowHalfHeight)][(int)(x+windowHalfWidth)] = true;
+				element[(int)(y+resize*3+windowHalfHeight)][(int)(x+windowHalfWidth)] = true;
+
+			}else if(type==TYPE_C){
+				element[(int)(y+windowHalfHeight)][(int)(x+windowHalfWidth)] = true;
+				element[(int)(y+windowHalfHeight)][(int)((x+resize)+windowHalfWidth)] = true;
+				element[(int)((y+resize)+windowHalfHeight)][(int)(x+windowHalfWidth)] = true;
+				element[(int)((y-resize)+windowHalfHeight)][(int)((x+resize)+windowHalfWidth)] = true;
+
+			}else if(type==TYPE_D){
+				element[(int)(y+windowHalfHeight)][(int)(x+windowHalfWidth)] = true;
+				element[(int)(y+windowHalfHeight)][(int)((x+resize)+windowHalfWidth)] = true;
+				element[(int)((y-resize)+windowHalfHeight)][(int)((x-resize)+windowHalfWidth)] = true;
+				element[(int)((y-resize)+windowHalfHeight)][(int)(x+windowHalfWidth)] = true;
+
+			}else if(type==TYPE_E){
+				element[(int)(y+windowHalfHeight)][(int)(x+windowHalfWidth)] = true;
+				element[(int)(y+windowHalfHeight)][(int)((x-resize)+windowHalfWidth)] = true;
+				element[(int)((y-resize)+windowHalfHeight)][(int)((x-resize)+windowHalfWidth)] = true;
+				element[(int)((y+resize)+windowHalfHeight)][(int)(x+windowHalfWidth)] = true;
+
+			}else if(type==TYPE_F){
+				element[(int)(y+windowHalfHeight)][(int)(x+windowHalfWidth)] = true;
+				element[(int)(y+windowHalfHeight)][(int)((x-resize)+windowHalfWidth)] = true;
+				element[(int)((y-resize)+windowHalfHeight)][(int)(x+windowHalfWidth)] = true;
+				element[(int)((y-resize)+windowHalfHeight)][(int)((x+resize)+windowHalfWidth)] = true;
+
+			}else if(type==TYPE_G){
+				element[(int)(y+windowHalfHeight)][(int)(x+windowHalfWidth)] = true;
+				element[(int)(y+windowHalfHeight)][(int)((x+resize)+windowHalfWidth)] = true;
+				element[(int)((y+resize)+windowHalfHeight)][(int)(x+windowHalfWidth)] = true;
+				element[(int)((y+resize)+windowHalfHeight)][(int)((x+resize)+windowHalfWidth)] = true;
+
 			}
 			x = 0;
 			y = COL/2;
+
 			selectType();//select block style ;
 			break;
 	}
@@ -252,22 +516,63 @@ void animationTimer(int speed){
 void renderScene(void){
 
 	glClear(GL_COLOR_BUFFER_BIT);
-	glColor3f(0.0,1.0,0.0);
+	glColor3f(0.99,0.1,0.0);
 
 	// Moving object
 	if(type == TYPE_A){
 		//"けけ" "ぱ" shape
 		//draw object using vertex.
 		glRectf(x,y,x+resize,y+resize);
-		glRectf(x+resize,y,x+2*resize,y+resize);
+		glRectf(x+resize,y,x+resize*2, y+resize);
+		glRectf(x+resize*2,y, x+resize*3, y+resize);
+		glRectf(x+resize*3,y, x+resize*4, y+resize);
 
 	}else if(type == TYPE_B){
-		//"け"
-		//"け" shape
+		//"l" shape
 		//draw object using vertext.
 		glRectf(x,y,x+resize,y+resize);
 		glRectf(x,y+resize,x+resize,y+resize*2);
+		glRectf(x,y+resize*2,x+resize,y+resize*3);
+		glRectf(x,y+resize*3,x+resize,y+resize*4);
 
+	}else if(type == TYPE_C){
+		//"L" shape
+		//"ぁ" shape
+		glRectf(x,y,x+resize,y+resize);
+		glRectf(x+resize,y,x+resize*2,y+resize);
+		glRectf(x,y+resize,x+resize,y+resize*2);
+		glRectf(x+resize,y-resize,x+resize*2,y);
+
+	}else if(type == TYPE_D){
+		//" けけ" shape
+		//"けけ " shape
+		glRectf(x,y,x+resize,y+resize);
+		glRectf(x+resize,y,x+resize*2,y+resize);
+		glRectf(x,y-resize,x+resize,y);
+		glRectf(x-resize,y-resize,x,y);
+	}else if(type == TYPE_E){
+		//"  け" shape
+		//"け け " shape
+		//"け     " shape
+		glRectf(x,y,x+resize,y+resize);
+		glRectf(x-resize,y,x,y+resize);
+		glRectf(x,y+resize,x+resize,y+resize*2);
+		glRectf(x-resize,y-resize,x,y);
+	}else if(type == TYPE_F){
+		//"けけ" shape
+		//" けけ " shape
+		glRectf(x,y,x+resize,y+resize);
+		glRectf(x-resize,y,x,y+resize);
+		glRectf(x,y-resize,x+resize,y);
+		glRectf(x+resize,y-resize,x+resize*2,y);
+
+	}else if(type == TYPE_G){
+		//"けけ" shape
+		//"けけ " shape
+		glRectf(x,y,x+resize,y+resize);
+		glRectf(x+resize,y,x+resize*2,y+resize);
+		glRectf(x,y+resize,x+resize,y+resize*2);
+		glRectf(x+resize,y+resize,x+resize*2,y+resize*2);
 	}
 
 
@@ -296,10 +601,10 @@ int main(int argc, char**argv){
 	glutCreateWindow("Tetris");
 	glutDisplayFunc(renderScene);
 	glutTimerFunc(SPEED, animationTimer, 1);
+	glutKeyboardFunc(normalKey);
 	glutSpecialFunc(specialKey);
 	glutReshapeFunc(changeSize);
 	setupRC();
-
 	glutMainLoop();
 	return 0;
 
